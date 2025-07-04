7e01e785b803464599fdd7bfc619b43274cd69f0
Bytesbanana <saharat.paynok@gmail.com>
2025-07-05 02:17:59 +0700

    feat: add new textures and optimize App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { CameraControls } from "@react-three/drei";
+import { Canvas, useLoader } from "@react-three/fiber";
+import { TextureLoader } from "three";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [terrianMap, depthMap] = useLoader(TextureLoader, [
+    "terrain_z1.jpg",
+    "depth_z1.jpg",
+  ]);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Canvas camera={{ position: [0, 0, 5], fov: 50 }}>
+        <CameraControls />
+        <ambientLight intensity={3} />
+        <pointLight position={[10, 10, 10]} />
+        <mesh>
+          <planeGeometry args={[10, 10, 256, 256]} />{" "}
+          {/* Add segments for smooth displacement */}
+          <meshStandardMaterial
+            map={terrianMap}
+            displacementMap={depthMap}
+            displacementScale={1} // Adjust for desired depth effect
+          />
+        </mesh>
+      </Canvas>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
