0a7a8eafb27752c1743c220f43567877ff1452b2
Bytesbanana <saharat.paynok@gmail.com>
2025-07-05 14:29:55 +0700

    refactor: replace CSS file and improve camera controls

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,49 @@
-import { CameraControls } from "@react-three/drei";
 import { Canvas, useLoader } from "@react-three/fiber";
-import { TextureLoader } from "three";
+import { useEffect, useRef } from "react";
+import * as THREE from "three"; //
+import styles from "./App.module.css";
+import { PerspectiveCamera } from "@react-three/drei";
 
 const App = () => {
-  const [terrianMap, depthMap] = useLoader(TextureLoader, [
+  const [terrianMap, depthMap] = useLoader(THREE.TextureLoader, [
     "terrain_z1.jpg",
     "depth_z1.jpg",
   ]);
 
+  const cameraRef = useRef<THREE.PerspectiveCamera>(null);
+
+  useEffect(() => {
+    const wheelEvent = (e: WheelEvent) => {
+      e.stopPropagation();
+      // console.log("Wheel event detected", e.deltaY);
+
+      if (cameraRef.current) {
+        const distant = e.deltaY * 0.001;
+        if (cameraRef.current.position.z > 1.5) {
+          cameraRef.current.translateZ(distant);
+          cameraRef.current.updateProjectionMatrix();
+        }
+      }
+    };
+    window.addEventListener("wheel", wheelEvent);
+    return () => {
+      window.removeEventListener("wheel", wheelEvent);
+    };
+  }, []);
+
   return (
-    <>
-      <Canvas camera={{ position: [0, 0, 5], fov: 50 }}>
-        <CameraControls />
+    <div className={styles.container}>
+      <Canvas>
+        <PerspectiveCamera
+          ref={cameraRef}
+          makeDefault
+          position={[0, -0.5, 5]}
+          fov={50}
+        />
         <ambientLight intensity={3} />
         <pointLight position={[10, 10, 10]} />
         <mesh>
-          <planeGeometry args={[10, 10, 256, 256]} />{" "}
-          {/* Add segments for smooth displacement */}
+          <planeGeometry args={[10, 10, 256, 256]} /> <axesHelper />
           <meshStandardMaterial
             map={terrianMap}
             displacementMap={depthMap}
@@ -24,7 +51,7 @@ const App = () => {
           />
         </mesh>
       </Canvas>
-    </>
+    </div>
   );
 };
 
