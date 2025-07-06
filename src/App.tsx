4b72dfbba37cde9ea66f3b3d37c3d12793de2a68
Bytesbanana <saharat.paynok@gmail.com>
2025-07-06 10:11:52 +0700

    feat: add zoom limits and configuration for textures

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,11 @@ import { Canvas, useLoader } from "@react-three/fiber";
 import { useEffect, useRef } from "react";
 import * as THREE from "three"; //
 import styles from "./App.module.css";
-import { PerspectiveCamera } from "@react-three/drei";
+import { PerspectiveCamera, Stats } from "@react-three/drei";
+import { DEFAULT_ZOOM, MAX_ZOOM, MIN_ZOOM, TILES } from "./config";
 
 const App = () => {
-  const [terrianMap, depthMap] = useLoader(THREE.TextureLoader, [
-    "terrain_z1.jpg",
-    "depth_z1.jpg",
-  ]);
+  const [terrianMap, depthMap] = useLoader(THREE.TextureLoader, TILES);
 
   const cameraRef = useRef<THREE.PerspectiveCamera>(null);
 
@@ -16,13 +14,16 @@ const App = () => {
     const wheelEvent = (e: WheelEvent) => {
       e.stopPropagation();
 
-      if (cameraRef.current) {
-        const distant = e.deltaY * 0.001;
-        if (cameraRef.current.position.z > 1.5) {
-          cameraRef.current.translateZ(distant);
-          cameraRef.current.updateProjectionMatrix();
-        }
+      if (!cameraRef.current) return;
+      const distant = e.deltaY * 0.001;
+      cameraRef.current.translateZ(distant);
+      if (cameraRef.current.position.z < MIN_ZOOM) {
+        cameraRef.current.position.z = MIN_ZOOM;
       }
+      if (cameraRef.current.position.z > MAX_ZOOM) {
+        cameraRef.current.position.z = MAX_ZOOM;
+      }
+      cameraRef.current.updateProjectionMatrix();
     };
     window.addEventListener("wheel", wheelEvent);
     return () => {
@@ -36,19 +37,20 @@ const App = () => {
         <PerspectiveCamera
           ref={cameraRef}
           makeDefault
-          position={[0, -0.5, 5]}
+          position={[0, -0.25, DEFAULT_ZOOM]}
           fov={50}
         />
         <ambientLight intensity={3} />
         <pointLight position={[10, 10, 10]} />
         <mesh>
-          <planeGeometry args={[10, 10, 256, 256]} /> <axesHelper />
+          <planeGeometry args={[10, 10, 256, 256]} />
           <meshStandardMaterial
             map={terrianMap}
             displacementMap={depthMap}
-            displacementScale={1} // Adjust for desired depth effect
+            displacementScale={1}
           />
         </mesh>
+        <Stats />
       </Canvas>
     </div>
   );
---
+++ src/config.ts
@@ -0,0 +1,4 @@
+export const TILES = ["terrain_z1.jpg", "depth_z1.jpg"];
+export const MIN_ZOOM = 1.75;
+export const DEFAULT_ZOOM = 4.5;
+export const MAX_ZOOM = 5.6;
